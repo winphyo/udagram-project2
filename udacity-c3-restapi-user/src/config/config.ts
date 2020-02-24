8f5b771f97c0a396302b4fcf233f46afbfafcc83
Win Phyo <wpthein@gmail.com>
2020-02-24 17:43:10 +0630

    Initial Commit Travic CI

---
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -0,0 +1,26 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    //"username": process.env.POSTGRESS_USERNAME,
+    "password": "postgres",
+    "database": "udagramdev",
+    "host": "udagramdev.chhjjps1fatp.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "url": "http://192.168.99.101:8100",
+    "aws_media_bucket": "udagram-win-dev"
+  },
+  "prod": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "udagramdev",
+    "host": "udagramdev.chhjjps1fatp.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres"
+  },
+  "jwt" :
+  {
+    "secret" : "winphyothein"
+  }
+
+}
